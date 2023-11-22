3049b9671892ccb985d7219dfe15acf4ab008ecd
Matheus Pimentel <matheus08.pimentel@gmail.com>
2023-11-21 21:20:58 -0300

    Worked on the sidebar behavior while collapsed

--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -7,28 +7,35 @@ export interface SidebarItemProps {
   Icon: LucideIcon;
   label: string;
   rightText?: string;
+  iconOnly?: boolean;
 }
 
 export const SidebarItem: FC<SidebarItemProps> = (props) => {
-  const { isActive, onClick, Icon, label, rightText } = props;
+  const { isActive, onClick, Icon, label, rightText, iconOnly } = props;
 
   return (
     <div
       className={`${
         isActive
-          ? "bg-purple-1 text-white"
+          ? "bg-purple-1 select-none text-white"
           : "bg-blue-3 cursor-pointer text-gray-6 hover:text-white"
-      } hover:bg-purple-1 flex items-center gap-4 rounded-lg p-3 transition-colors duration-200 ease-in-out`}
+      } hover:bg-purple-1 flex ${
+        iconOnly ? "w-fit" : "w-full"
+      } items-center gap-4 rounded-lg p-3 transition-colors duration-200 ease-in-out`}
       onClick={() => onClick()}
     >
       <Icon size={20} />
 
-      <span>{label}</span>
+      {!iconOnly && (
+        <>
+          <span>{label}</span>
 
-      {rightText && (
-        <span className="bg-blue-1 text-purple-3 ml-auto flex h-5 w-5 items-center justify-center rounded-full text-xs font-semibold">
-          {rightText}
-        </span>
+          {rightText && (
+            <span className="bg-blue-1 text-purple-3 ml-auto flex h-5 w-5 items-center justify-center rounded-full text-xs font-semibold">
+              {rightText}
+            </span>
+          )}
+        </>
       )}
     </div>
   );
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { FC } from "react";
+import { FC, useState } from "react";
 import { SidebarItem, SidebarItemProps } from "./SidebarItem";
 import {
+  ChevronsLeft,
   Flag,
   GraduationCap,
   LayoutDashboard,
@@ -18,6 +19,8 @@ interface SidebarProps {}
 
 export const Sidebar: FC<SidebarProps> = (props) => {
   const {} = props;
+
+  const [isSidebarExpanded, setIsSidebarExpanded] = useState(true);
 
   const { push } = useRouter();
   const pathName = usePathname();
@@ -68,8 +71,27 @@ export const Sidebar: FC<SidebarProps> = (props) => {
   ];
 
   return (
-    <aside className="bg-blue-1 h-full w-64 px-4 py-6">
-      <section className="mb-6 flex flex-col items-center gap-4">
+    <aside
+      className={`${
+        isSidebarExpanded ? "w-64" : "w-20"
+      } bg-blue-1 transition-width relative h-full px-4 py-6 duration-500 ease-in-out`}
+    >
+      <div
+        className={`${
+          isSidebarExpanded ? "right-3" : "right-1/2 translate-x-1/2 rotate-180"
+        } hover:bg-blue-3 absolute top-3 cursor-pointer rounded-full p-1 text-gray-6 transition-colors hover:text-gray-7`}
+      >
+        <ChevronsLeft
+          size={22}
+          onClick={() => setIsSidebarExpanded(!isSidebarExpanded)}
+        />
+      </div>
+
+      <section
+        className={`${
+          !isSidebarExpanded ? "pt-8" : ""
+        } mb-6 flex flex-col items-center gap-4 duration-200 ease-in-out`}
+      >
         <Image
           className="rounded-full"
           src={avatarExample}
@@ -78,28 +100,42 @@ export const Sidebar: FC<SidebarProps> = (props) => {
           height={100}
         />
 
-        <div className="text-center">
+        <div
+          className={`${
+            isSidebarExpanded
+              ? ""
+              : "invisible h-0 opacity-0 delay-0 duration-150"
+          } text-center transition-opacity delay-300 duration-500`}
+        >
           <p className="text-xl text-gray-6">Matheus Pimentel</p>
           <p className="mt-1 text-lg text-gray-5">Teacher</p>
         </div>
       </section>
 
       <section>
-        <p className="mb-4">My Space</p>
+        {isSidebarExpanded && <p className="mb-4">My Space</p>}
 
         <div className="flex flex-col gap-4">
           {mySpaceSidebarItems.map((sidebarItem) => (
-            <SidebarItem key={sidebarItem.label} {...sidebarItem} />
+            <SidebarItem
+              key={sidebarItem.label}
+              {...sidebarItem}
+              iconOnly={!isSidebarExpanded}
+            />
           ))}
         </div>
       </section>
 
-      <section className="border-blue-3 mt-6 border-t-2 pt-4 ">
-        <p className="mb-4">My Students</p>
+      <section className="border-blue-3 mt-6 border-t-2 pt-4">
+        {isSidebarExpanded && <p className="mb-4">My Students</p>}
 
         <div className="flex flex-col gap-4">
           {myStudentsSidebarItems.map((sidebarItem) => (
-            <SidebarItem key={sidebarItem.label} {...sidebarItem} />
+            <SidebarItem
+              key={sidebarItem.label}
+              {...sidebarItem}
+              iconOnly={!isSidebarExpanded}
+            />
           ))}
         </div>
       </section>
